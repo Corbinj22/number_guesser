ad34bdd55f5f2992fd9161b46337c71cb7eae9bf
Justin Corbin <jcorbin2290@gmail.com>
2019-12-07 16:11:58 -0700

    player 1 and 2 guess and name display, and form clears

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function toggle() {
 }
 
 console.log(updateButton);
+
+
+// display player name and guess
+  function showPlayerGuess() {
+    document.getElementById('player-1-guess-display').innerText =
+    document.getElementById('guess1').value;
+    document.getElementById('player-2-guess-display').innerText =
+    document.getElementById('guess2').value;
+    document.getElementById('player-1-name-display').innerText =
+    document.getElementById('player-name1').value;
+    document.getElementById('player-2-name-display').innerText =
+    document.getElementById('player-name2').value;
+    document.getElementById('guess1').value='';
+    document.getElementById('guess2').value='';
+    document.getElementById('player-name1').value='';
+    document.getElementById('player-name2').value='';
+}
