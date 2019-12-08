f65cfe72809904a9f99a454343b2385e66e82b90
Kimala Cochran <kimalajoy@gmail.com>
2019-12-08 13:56:33 -0700

    Updated functionality for Iteration 1 forms and buttons

--- main.js
+++ main.js
@@ -1,35 +1,59 @@
 // Set Range Box
-// var inputField = document.querySelector("#min");
-// var inputField = document.querySelector("#max");
-// var updateButton = document.querySelector("#updateButton");
+// var inputField = document.querySelector('#min');
+// var inputField = document.querySelector('#max');
+// var updateButton = document.querySelector('#updateButton');
 //
 // updateButton.disabled = true;
-// inputField.addEventListener("input", toggle);
+// inputField.addEventListener('input', toggle);
 //
 // function toggle() {
 //   updateButton.disabled = inputField.value != '' ? false : true;
 // }
 
+var submitEnabled = false;
+var clearEnabled = false;
+
 // Mid-Challenger-box - Challenger1
-var inputField = document.querySelector("#name1");
-var inputField = document.querySelector("#guess1");
-var updateButton = document.querySelector("#submit");
+var challenger1Name = document.querySelector('#challenger1Name');
+var challenger1Guess = document.querySelector('#challenger1Guess');
+var challenger2Name = document.querySelector('#challenger2Name');
+var challenger2Guess = document.querySelector('#challenger2Guess');
+var submitButton = document.querySelector('#submit');
+var resetButton = document.querySelector('#reset');
+var clearButton = document.querySelector('#clear');
 
-updateButton.disabled = true;
-inputField.addEventListener("input", toggle);
+challenger1Name.addEventListener('input', validateInputs);
+challenger1Guess.addEventListener('input', validateInputs);
+challenger2Name.addEventListener('input', validateInputs);
+challenger2Guess.addEventListener('input', validateInputs);
 
-function toggle() {
-  updateButton.disabled = inputField.value != '' ? false : true;
+submitButton.addEventListener('click', submitGuesses);
+clearButton.addEventListener('click', clearGuesses);
+
+function validateInputs(){
+  submitEnabled = (challenger1Name.value
+    && challenger1Guess.value
+    && challenger2Name.value
+    && challenger2Guess.value) ? true : false;
+
+    clearEnabled = (challenger1Name.value
+      || challenger1Guess.value
+      || challenger2Name.value
+      || challenger2Guess.value) ? true : false;
+
+  submitButton.disabled = submitEnabled ? '' : 'disabled';
+  clearButton.disabled = clearEnabled ? '' : 'disabled';
 }
-// Mid-Challenger-box - Challenger2
-var inputField = document.querySelector("#name2");
-var inputField = document.querySelector("#guess2");
-var updateButton = document.querySelector("#submit");
 
-updateButton.disabled = true;
-inputField.addEventListener("input", toggle);
-
-function toggle() {
-  updateButton.disabled = inputField.value != '' ? false : true;
+function clearGuesses(){
+  challenger1Name.value = '';
+  challenger2Name.value = '';
+  challenger1Guess.value = '';
+  challenger2Guess.value = '';
+  validateInputs();
 }
-// console.log(updateButton);
+
+function submitGuesses(){
+  challenger1Guess.value = '';
+  challenger2Guess.value = '';
+}
