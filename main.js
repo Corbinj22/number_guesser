80de5b4f334c122c763f6b3a7254cdc8a82207b5
Justin Corbin <jcorbin2290@gmail.com>
2019-12-09 09:54:04 -0700

    ommit changes, merge to be on same master branch

--- main.js
+++ main.js
@@ -1,65 +1,53 @@
-// Set Range Box
-// var minRange = document.querySelector('#min');
-// var maxRange = document.querySelector('#max');
-// var updateButton = document.querySelector('#updateButton');
-//
-// updateButton.disabled = true;
-// inputField.addEventListener('input', toggle);
-//
-// function toggle() {
-//   updateButton.disabled = inputField.value != '' ? false : true;
-// }
-
-// Mid-Challenger-box - Challenger1
+// Enable/Disable Buttons
 var submitEnabled = false;
 var clearEnabled = false;
 var updateEnabled = false;
-
+//Set range box update button
+var updateButton = document.querySelector('.updateButton');
+// Set Range Min Max Inputs
 var minRangeInput = document.querySelector('#min');
 var maxRangeInput = document.querySelector('#max');
+// Mid box names and guess input
 var challenger1Name = document.querySelector('#challenger1Name');
 var challenger1Guess = document.querySelector('#challenger1Guess');
 var challenger2Name = document.querySelector('#challenger2Name');
 var challenger2Guess = document.querySelector('#challenger2Guess');
+// mid box range value blanks
 var minRangeDisplay = document.querySelector('#minDisplay');
 var maxRangeDisplay = document.querySelector('#maxDisplay');
-
-var updateButton = document.querySelector('.updateButton');
+//mid box buttons
 var submitButton = document.querySelector('#submit');
 var resetButton = document.querySelector('#reset');
 var clearButton = document.querySelector('#clear');
-
+//Event listners
+// set range box input listners
 minRangeInput.addEventListener('input', validateRange);
 maxRangeInput.addEventListener('input', validateRange);
+// mid box unput listners for name and guesses
 challenger1Name.addEventListener('input', validateInputs);
 challenger1Guess.addEventListener('input', validateInputs);
 challenger2Name.addEventListener('input', validateInputs);
 challenger2Guess.addEventListener('input', validateInputs);
-
 submitButton.addEventListener('click', submitGuesses);
 clearButton.addEventListener('click', clearGuesses);
 updateButton.addEventListener('click', setCurrentRange);
-
+// mid box validation and clear/select button
 function validateInputs(){
   submitEnabled = (challenger1Name.value
     && challenger1Guess.value
     && challenger2Name.value
     && challenger2Guess.value) ? true : false;
-
     clearEnabled = (challenger1Name.value
       || challenger1Guess.value
       || challenger2Name.value
       || challenger2Guess.value) ? true : false;
-
   submitButton.disabled = submitEnabled ? '' : 'disabled';
   clearButton.disabled = clearEnabled ? '' : 'disabled';
 }
-
 function validateRange() {
   updateEnabled = (minRangeInput.value && maxRangeInput.value)  ? true : false;
   updateButton.disabled = updateEnabled ? '' : 'disabled';
 }
-
 function clearGuesses(){
   challenger1Name.value = '';
   challenger2Name.value = '';
@@ -73,13 +61,10 @@ function submitGuesses(){
   challenger2Guess.value = '';
   validateInputs();
 }
-
 function setCurrentRange() {
   minRangeDisplay.innerText = minRangeInput.value;
   maxRangeDisplay.innerText = maxRangeInput.value;
 }
-
-
 // display player name and guess
   function showPlayerGuess() {
     document.getElementById('player-1-guess-display').innerText = challenger1Guess.value;
@@ -87,3 +72,11 @@ function setCurrentRange() {
     document.getElementById('player-1-name-display').innerText = challenger1Name.value;
     document.getElementById('player-2-name-display').innerText = challenger2Name.value;
   }
+// display guess high/low
+var hardNumer = 5
+function comparePlayerGuess(){
+document.getElementById('player-1-guess-display').innerText
+}
+// take user input
+// compare user input to hard value of number
+// display too high or too low
